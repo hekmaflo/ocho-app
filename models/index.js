53e02aee6d49034e91939fdf97d75f90b060c72a
Hector Flores <106941903+hekmaflo@users.noreply.github.com>
2023-03-28 13:08:59 -0500

    added schemas to index

--- models/index.js
+++ models/index.js
@@ -1,11 +1,17 @@
 const mongoose = require("mongoose");
 const UserSchema = require("./user");
 const ProfileSchema = require("./profile");
+const ListSchema = require("./list");
+const ItemSchema = require("./item");
 
 const User = mongoose.model("User", UserSchema);
 const Profile = mongoose.model("Profile", ProfileSchema);
+const List = mongoose.model("List", ListSchema);
+const Item = mongoose.model("Item", ItemSchema);
 
 module.exports = {
   User,
   Profile,
+  List,
+  Item,
 };
