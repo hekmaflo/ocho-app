3c5363e7ec8da6f2c957121d2d38fe1b57d0129e
Hector Flores <106941903+hekmaflo@users.noreply.github.com>
2023-03-31 13:13:41 -0500

    added form and axios post call for user registration

--- client/src/components/UserRegister.jsx
+++ client/src/components/UserRegister.jsx
@@ -1,28 +1,80 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const UserRegister = () => {
+  const [formValues, setFormValues] = useState({
+    name: "",
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormValues({ ...formValues, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      await axios.post("/api/register", formValues);
+      navigate("/home");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div>
         <div>
-          <label>First Name:</label>
-          <input type="text" />
+          <label>Name:</label>
+          <input
+            type="text"
+            name="name"
+            value={formValues.name}
+            onChange={handleChange}
+          />
         </div>
         <div>
-          <label>Last Name:</label>
-          <input type="text" />
+          <label>User Name:</label>
+          <input
+            type="text"
+            name="username"
+            value={formValues.username}
+            onChange={handleChange}
+          />
         </div>
         <div>
           <label>Email:</label>
-          <input type="text" />
+          <input
+            type="email"
+            name="email"
+            value={formValues.email}
+            onChange={handleChange}
+          />
         </div>
         <div>
           <label>Password:</label>
-          <input type="text" />
+          <input
+            type="password"
+            name="password"
+            value={formValues.password}
+            onChange={handleChange}
+          />
         </div>
         <div>
-          <label>Retype Password:</label>
-          <input type="text" />
+          <label>Confirm Password:</label>
+          <input
+            type="password"
+            name="confirmPassword"
+            value={formValues.confirmPassword}
+            onChange={handleChange}
+          />
         </div>
       </div>
       <div>
