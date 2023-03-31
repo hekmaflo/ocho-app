b4b218945cdffadde1c7f18185f6524f21237821
Hector Flores <106941903+hekmaflo@users.noreply.github.com>
2023-03-31 14:04:10 -0500

    update

--- server.js
+++ server.js
@@ -2,8 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const routes = require("./routes");
 const db = require("./db");
-
-// require() imports and middleware here ^ ///////
+const cors = require("cors");
 
 const PORT = process.env.PORT || 3001;
 
@@ -11,8 +10,7 @@ const app = express();
 
 app.use(express.json());
 
-// app.use() middleware here ^ ///////////////////
-
+app.use(cors());
 app.use("/api", routes);
 
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
