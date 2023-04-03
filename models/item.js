dfee79a5c6e1e1d24a96b00ef3406485590e243f
Hector Flores <106941903+hekmaflo@users.noreply.github.com>
2023-04-02 22:25:11 -0500

    installed cloudinary

--- models/item.js
+++ models/item.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const Item = new Schema({
-  title: { type: String, required: true },
+  title: { type: String, required: false },
   description: { type: String, required: false },
   image: { type: String, required: false },
   url: { type: String, required: false },
